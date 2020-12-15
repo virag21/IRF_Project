1db9962668129cda7359e9e19f208475f4016315
Virág Ványi <virag.vanyi@stud.uni-corvinus.hu>
2020-12-15 19:21:32 +0100

    excel in progress

--- IRF_project_GXMRLU/IRF_project_GXMRLU/UploadDataUserControl.Designer.cs
+++ IRF_project_GXMRLU/IRF_project_GXMRLU/UploadDataUserControl.Designer.cs
@@ -31,12 +31,13 @@
             this.chooseFile = new System.Windows.Forms.Button();
             this.exportBtn = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // chooseFile
             // 
-            this.chooseFile.Location = new System.Drawing.Point(83, 60);
+            this.chooseFile.Location = new System.Drawing.Point(83, 78);
             this.chooseFile.Name = "chooseFile";
             this.chooseFile.Size = new System.Drawing.Size(158, 41);
             this.chooseFile.TabIndex = 0;
@@ -46,12 +47,13 @@
             // 
             // exportBtn
             // 
-            this.exportBtn.Location = new System.Drawing.Point(83, 127);
+            this.exportBtn.Location = new System.Drawing.Point(83, 145);
             this.exportBtn.Name = "exportBtn";
             this.exportBtn.Size = new System.Drawing.Size(158, 36);
             this.exportBtn.TabIndex = 1;
             this.exportBtn.Text = "Excel export";
             this.exportBtn.UseVisualStyleBackColor = true;
+            this.exportBtn.Visible = false;
             this.exportBtn.Click += new System.EventHandler(this.exportBtn_Click);
             // 
             // dataGridView1
@@ -64,10 +66,20 @@
             this.dataGridView1.Size = new System.Drawing.Size(504, 285);
             this.dataGridView1.TabIndex = 2;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "back to Main";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // UploadDataUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.exportBtn);
             this.Controls.Add(this.chooseFile);
@@ -83,5 +95,6 @@
         private System.Windows.Forms.Button chooseFile;
         private System.Windows.Forms.Button exportBtn;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- IRF_project_GXMRLU/IRF_project_GXMRLU/UploadDataUserControl.cs
+++ IRF_project_GXMRLU/IRF_project_GXMRLU/UploadDataUserControl.cs
@@ -54,7 +54,7 @@ namespace IRF_project_GXMRLU
                 dataGridView1.DataSource = OrderList;
             }
 
-
+            exportBtn.Visible = true;
         }
 
         Excel.Application xlApp;
@@ -73,7 +73,7 @@ namespace IRF_project_GXMRLU
 
                 xlSheet = xlWB.ActiveSheet;
 
-                CreateTable(); // Ennek megírása a következő feladatrészben következik
+                CreateTable();
 
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
@@ -89,7 +89,7 @@ namespace IRF_project_GXMRLU
                 xlApp = null;
             }
 
-
+            FormatTable();
 
         }
 
@@ -109,19 +109,23 @@ namespace IRF_project_GXMRLU
 
             return ExcelCoordinate;
         }
-
-        private void CreateTable()
-        {
-            string[] headers = new string[] {
+        string[] headers = new string[] {
                          "Sender",
+                         "SenderPhone",
                          "SenderEmail",
                          "Name",
-                         "Kerület",
-                         "Email",
                          "Address",
                          "Gift",
                          "Quantity"};
+        private void CreateTable()
+        {
+            
 
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+
+            }
             object[,] values = new object[OrderList.Count, headers.Length];
             int counter = 0;
             foreach (Order f in OrderList)
@@ -133,13 +137,29 @@ namespace IRF_project_GXMRLU
                 values[counter, 4] = f.Address;
                 values[counter, 5] = f.Gift;
                 values[counter, 6] = f.Quantity;
-                values[counter, 7] = "";
                 counter++;
             }
             xlSheet.get_Range(
-GetCell(2, 1),
-GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+                GetCell(2, 1),
+                GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
 
         }
+        private void FormatTable()
+        {
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            Excel.Range fullRange = xlSheet.get_Range(GetCell(1, 1), GetCell(xlSheet.UsedRange.Rows.Count, xlSheet.UsedRange.Columns.Count));
+            fullRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+            fullRange.Interior.Color = Color.Wheat;
+
+        }
+
+
     }
 }
